39c7c1d11e3057c97d8f3859e100c2ea5c11ea89
NamDao <bk.namapple@gmail.com>
2025-01-10 21:56:31 +0700

    Init project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+    }
+
+    public void ClickedOnGridPosition()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class GridPosition : MonoBehaviour
+{
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+    
+    
+    
+    private void OnMouseDown()
+    {
+        
+    }
+}
